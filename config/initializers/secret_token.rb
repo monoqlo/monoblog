c0304d92b91ec397bfc7a960bd16c253a877f782
monoqlo <monoqlo44@gmail.com>
2012-04-25 14:24:59 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monoblog::Application.config.secret_token = '6ecf112bc2752cc8c38e9faa60908625ed448fbcf4f2c70db500181903b9e509c49e741f8918b624151f3707cb64504cb9f1e4f12dec983b15f35c94e8c7cd2f'
